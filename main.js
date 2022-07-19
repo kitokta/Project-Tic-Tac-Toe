054a43afc00051333ba3fdb855c4b9ab23651a7d
Gabi Zanini <psicologia.plus@gmail.com>
2022-07-19 13:53:03 -0300

    gameboard module and Ui task class

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-//GAMEBOARD CLASS
-class gameBoard {
-    display = (() => {
 
-    }) 
+
+//GAMEBOARD MODULE
+const gameBoard = (() => {
+    const create = () => {
+        const board = document.getElementsByClassName('board-square')
+        return board;
+    }
+    return {create}
+    })() 
+     
+//UI TASKS CLASS
+class uiTask {
+    static displayBoard() {
+        gameBoard.create;
+        const boardBox = document.getElementById('game-board');
+        boardBox.classList.add("show");
+    }
+
+
 }
+
+//uiTask.displayBoard()
+
 //PLAYER CLASS
 
 //EVENT: START GAME
