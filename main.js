6d893a69ffd3d76e9fc6f27c10ac4688996a9b08
Gabi Zanini <psicologia.plus@gmail.com>
2022-07-19 15:49:37 -0300

    Added player and ia constructors via prompt

--- main.js
+++ main.js
@@ -1,35 +1,67 @@
-
-
 //GAMEBOARD MODULE
 const gameBoard = (() => {
     const create = () => {
-        const board = document.getElementsByClassName('board-square')
+        const board = document.getElementsByClassName('board-square');
         return board;
     }
-    return {create}
-    })() 
+    return { create };
+})(); 
      
 //UI TASKS CLASS
 class uiTask {
-    static displayBoard() {
-        gameBoard.create;
+    static game(human, IA) {
+        const board = gameBoard.create();
         const boardBox = document.getElementById('game-board');
         boardBox.classList.add("show");
+        // for (const square of board) {
+        //     square.addEventListener('click', (e) => {
+        //         square.innerText = "O";
+        //     });
+        // }
+        // ou
+        for (let i = 0; i < board.length; i++) {
+            board[i].addEventListener('click', (e) => {
+                if(board[i].innerText==""){
+                board[i].innerText = `${human.marker}`;
+                }
+            });
+        }
     }
-
-
 }
 
 
 
 //PLAYER CLASS
+class Player {
+    constructor(name, marker) {
+        this.name = name;
+        this.marker = marker;
+    }
+}
+
 
 //EVENTS: START GAME
 const button = document.querySelector('button');
 const contentBox = document.querySelector('content');
 button.addEventListener('click', () => {
     button.remove();
-    uiTask.displayBoard();
+
+    //Human value inputs
+    let name = prompt("What's your name?")
+    let marker = prompt("Choose a marker: X or O")
+    if(marker!="X" || marker!="0") {
+        alert("Please insert a valid marker!");
+        setTimeout("location.reload(true);",1);
+    }
+    const human = new Player(name,marker);
+
+    //IA player inputs
+    let iaMarker = "";
+    if(marker=="X") iaMarker = "O";
+    else  iaMarker = "X";
+    const IA = new Player('machine', iaMarker);
+
+    //Starting Game
+    uiTask.game(human, IA);
 })
 
-//
