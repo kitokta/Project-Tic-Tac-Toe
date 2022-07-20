ad23c7f3d8185c6fed371d13d128629aaa77c354
Gabi Zanini <psicologia.plus@gmail.com>
2022-07-19 23:57:34 -0300

    IA test logic added

--- main.js
+++ main.js
@@ -5,18 +5,35 @@ const gameBoard = (() => {
         return board;
     }
 
-    const checkFields = (human, ia) => {
+    const checkFields = (human, IA) => {
+       //Human
         let humanIndex = [];
+        //For each cell that was played by the human player we push its index to another array so we can test all win cons later
         human.played.forEach((cell, index) => {
             if (cell === '1') {
                 humanIndex.push(index);
             }
         });
-            //Checking every possibility of win
+            //Checking every possibility of Human win
             if ((humanIndex[0] === 0 && humanIndex[1] === 3 && humanIndex[2] === 6)|| (humanIndex[0] === 1 && humanIndex[1] === 4 && humanIndex[2] === 7) || (humanIndex[0] === 2 && humanIndex[1] === 5 && humanIndex[2] === 8) || (humanIndex[0] === 0 && humanIndex[1] === 1 && humanIndex[2] === 2) || (humanIndex[0] === 3 && humanIndex[1] === 4 && humanIndex[2] === 5) || (humanIndex[0] === 6 && humanIndex[1] === 7 && humanIndex[2] === 8) || (humanIndex[0] === 0 && humanIndex[1] === 4 && humanIndex[2] === 8) || (humanIndex[0] === 2 && humanIndex[1] === 4 && humanIndex[2] === 6)) {
                 return setTimeout(alert(`${human.name} WINS! CONGRATULATIONS!`), 5),
                 setTimeout("location.reload(true);",1);
             }
+
+        //IA
+        let iaIndex = [];
+        //For each cell that was played by the IA player we push its index to another array so we can test all win cons later
+        IA.played.forEach((cell, index) => {
+        if (cell === '1') {
+            iaIndex.push(index);
+        }
+        });
+            //Checking every possibility of IA win
+            if ((iaIndex[0] === 0 && iaIndex[1] === 3 && iaIndex[2] === 6)|| (iaIndex[0] === 1 && iaIndex[1] === 4 && iaIndex[2] === 7) || (iaIndex[0] === 2 && iaIndex[1] === 5 && iaIndex[2] === 8) || (iaIndex[0] === 0 && iaIndex[1] === 1 && iaIndex[2] === 2) || (iaIndex[0] === 3 && iaIndex[1] === 4 && iaIndex[2] === 5) || (iaIndex[0] === 6 && iaIndex[1] === 7 && iaIndex[2] === 8) || (iaIndex[0] === 0 && iaIndex[1] === 4 && iaIndex[2] === 8) || (iaIndex[0] === 2 && iaIndex[1] === 4 && iaIndex[2] === 6)) {
+                return setTimeout(alert("THE MACHINE WON! TRY AGAIN!"), 5),
+                setTimeout("location.reload(true);",1);
+            }
+            
         
     }
 
@@ -36,15 +53,20 @@ class uiTask {
                     //Human Play
                     board[i].innerText = `${human.marker}`;
                     human.played[i] = "1";
+                    human.numberOfPlays++;
 
-                    //IA play
-                    let easyPlay = Math.floor((Math.random() * board.length) + 1);
-                    if(board[easyPlay].innerText==""){
-                    board[easyPlay].innerText = `${IA.marker}`;
-                    IA.played[easyPlay] = "1"
+                    //IA only stop playing when finds a blank space to play.
+                    while(IA.numberOfPlays<human.numberOfPlays){
+                        //IA easyPlay
+                        let easyPlay = Math.floor((Math.random() * board.length) + 1);
+                        if(board[easyPlay].innerText==""){
+                            board[easyPlay].innerText = `${IA.marker}`;
+                            IA.played[easyPlay] = "1"
+                            IA.numberOfPlays++;
+                        }
                     }
                 }
-                setTimeout(gameBoard.checkFields(human), 1000);
+                setTimeout(gameBoard.checkFields(human, IA), 1000);
             });
         }
     }
@@ -56,6 +78,7 @@ class Player {
         this.name = name;
         this.marker = marker;
         this.played = ['0', '0', '0', '0', '0', '0', '0', '0', '0'];
+        this.numberOfPlays = 0;
     }
 }
 
@@ -69,6 +92,7 @@ button.addEventListener('click', () => {
     //Human value inputs
     let name = prompt("What's your name?")
     let marker = prompt("Choose a marker: X or O")
+    marker = marker.toUpperCase();
     if(marker!="X" && marker!="O") {
         alert("Please insert a valid marker!");
         setTimeout("location.reload(true);",1);
