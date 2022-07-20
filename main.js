9b5cf128571af43ac69d3008c0cec3a768797a4f
Gabi Zanini <psicologia.plus@gmail.com>
2022-07-20 04:03:50 -0300

    First version

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ const gameBoard = (() => {
     const checkFields = (human, IA) => {
         //Wincons
         const winCons = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]];
-       
 
         //Human
         let humanIndex = [];
